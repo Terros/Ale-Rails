90ad50402e197dbdcc23684e20f43e697488687b
Terros <andofuentes@hotmail.com>
2016-07-29 14:19:22 -0600

    Comentario/articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,4 +13,11 @@ def new
 @article = Article.new
 end
 
+def create
+@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+
+@article.save
+redirect_to @article
+end
+
 end
